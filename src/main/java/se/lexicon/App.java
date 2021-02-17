df394337bf343e7da9c49a5b630f6ce1871589db
Eddie <eddie.ch.89d@gmail.com>
2021-02-17 16:38:49 +0100

    Connection to the database

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,12 +1,29 @@
 package se.lexicon;
 
 
+import se.lexicon.dao.db.DbConnection;
 
-public class App 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "--- Todo application ---" );
+
+        // test connection to the database
+        String query = "SELECT * FROM person";
+        try (
+                PreparedStatement preparedStatement = DbConnection.getConnection().prepareStatement(query)
+        ) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
