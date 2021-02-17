de999ef1f699a9112e4763dbfe1d3e3c7817cf0e
Eddie <eddie.ch.89d@gmail.com>
2021-02-17 16:51:37 +0100

    TodoItems Class

---
+++ src/main/java/se/lexicon/model/TodoItems.java
@@ -0,0 +1,16 @@
+package se.lexicon.model;
+
+
+
+import java.time.LocalDate;
+
+
+
+public class TodoItems {
+    private int todo_id;
+    private String title;
+    private String description;
+    private LocalDate deadline;
+    private int done;
+    private int assignee_id;
+}
