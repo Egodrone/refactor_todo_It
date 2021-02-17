911efceca01de4ffc8eeee46bf1a0e42754a2a4d
Eddie <eddie.ch.89d@gmail.com>
2021-02-17 16:53:42 +0100

    Person Class

---
+++ src/main/java/se/lexicon/model/Person.java
@@ -0,0 +1,68 @@
+package se.lexicon.model;
+
+
+import java.util.Objects;
+
+public class Person {
+    private int person_id;
+    private String first_name;
+    private String last_name;
+
+
+
+    public Person() {
+    }
+
+    public Person(int person_id, String first_name, String last_name) {
+        this.person_id = person_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public int getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return person_id == person.person_id && Objects.equals(first_name, person.first_name) && Objects.equals(last_name, person.last_name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(person_id, first_name, last_name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "person_id=" + person_id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                '}';
+    }
+
+}
